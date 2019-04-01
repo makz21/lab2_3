b1d2e201966bbf97b166ab7426a74da1c2a8b90e
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-04-01 22:53:23 +0200

    create test calculateJackardSimilarityWithEmptyIntersectionShouldReturnZero seq1!=seq2

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -9,14 +9,28 @@ import static org.junit.Assert.*;
 public class SimilarityFinderTest {
 
     private SequenceSearcherMock sequenceSearcherMock = new SequenceSearcherMock();
-
+    double similarity;
 
     @Test
     public void calculateJackardSimilaritySameSeqShoultReturnOne() {
+
         int[] seq1 = {1,2,3,4};
         int[] seq2 = {1,2,3,4};
+
         SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherMock());
-        double similarity = similarityFinder.calculateJackardSimilarity(seq1,seq2);
+        similarity = similarityFinder.calculateJackardSimilarity(seq1,seq2);
         assertThat(similarity, is(1.0));
     }
+
+    @Test
+    public void calculateJackardSimilarityWithEmptyIntersectionShouldReturnZero() {
+        int[] seq1 = {1, 2, 3};
+        int[] seq2 = {5, 7, 8};
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherMock());
+        similarity = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(similarity, is(0.0));
+    }
+
+
 }
