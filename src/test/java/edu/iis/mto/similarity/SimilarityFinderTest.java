d602459cb8bf354c29e97500da69e68089c98eec
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-04-01 22:58:25 +0200

    create calculateJackardSimilarityWithNotEmptyIntersectionShouldReturnValueBetweenZeroAndOne test

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.similarity;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,7 +8,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
-
     private SequenceSearcherMock sequenceSearcherMock = new SequenceSearcherMock();
     double similarity;
 
@@ -17,7 +17,7 @@ public class SimilarityFinderTest {
         int[] seq1 = {1,2,3,4};
         int[] seq2 = {1,2,3,4};
 
-        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherMock());
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherMock);
         similarity = similarityFinder.calculateJackardSimilarity(seq1,seq2);
         assertThat(similarity, is(1.0));
     }
@@ -27,9 +27,19 @@ public class SimilarityFinderTest {
         int[] seq1 = {1, 2, 3};
         int[] seq2 = {5, 7, 8};
 
-        SimilarityFinder similarityFinder = new SimilarityFinder(new SequenceSearcherMock());
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherMock);
         similarity = similarityFinder.calculateJackardSimilarity(seq1, seq2);
         assertThat(similarity, is(0.0));
+    }
+
+    @Test
+    public void calculateJackardSimilarityWithNotEmptyIntersectionShouldReturnValueBetweenZeroAndOne() {
+        int[] seq1 = {1, 2, 3};
+        int[] seq2 = {3, 6, 8};
+
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherMock);
+        similarity = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(similarity, is(0.2));
     }
 
 
