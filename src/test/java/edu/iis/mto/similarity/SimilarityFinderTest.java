abbfc617db60300eb97df0b044ed3bc1d2d88cc1
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-04-01 22:37:14 +0200

    create SimilarityFinderTest

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,16 @@
+package edu.iis.mto.similarity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class SimilarityFinderTest {
+
+    private SequenceSearcherMock sequenceSearcherMock = new SequenceSearcherMock();
+
+
+    @Test public void calculateJackardSimilarity() {
+    }
+}
