56c1abd3f2acebdd2eb5325dd868d79dc610ee26
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-04-01 23:05:36 +0200

    optimize imports

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,11 +1,9 @@
 package edu.iis.mto.similarity;
 
-import org.junit.Before;
-import org.junit.Test;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
     private SequenceSearcherMock sequenceSearcherMock = new SequenceSearcherMock();
